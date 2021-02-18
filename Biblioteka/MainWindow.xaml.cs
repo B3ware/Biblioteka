af347a52bf5fea800459e3504b6ebc9310953733
B3ware <burek.pawel4@gmail.com>
2021-02-18 22:18:42 +0100

    Logowanie

--- Biblioteka/MainWindow.xaml.cs
+++ Biblioteka/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,58 @@ namespace Biblioteka
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public string conString = "Data Source=DESKTOP-H9HITU2;Initial Catalog=Biblioteka;Integrated Security=True";
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(conString);
+            try
+            {
+
+                if (con.State == System.Data.ConnectionState.Closed)
+                    con.Open();
+
+
+                string pozycja = "Czytelnicy";
+                if ((bool)pracownik.IsChecked)
+                    pozycja = "Pracownicy";
+
+                string query = "SELECT COUNT(1) FROM " + pozycja + " WHERE Login='" + txtLogin.Text + "' AND Haslo='" + txtHaslo.Text + "'";
+                SqlCommand sqlCmd = new SqlCommand(query, con);
+                sqlCmd.CommandType = CommandType.Text;
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Menu menu = new Menu();
+                    menu.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Podaj poprawne hasło");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Biblioteka.BibliotekaDataSet bibliotekaDataSet = ((Biblioteka.BibliotekaDataSet)(this.FindResource("bibliotekaDataSet")));
+            // Załaduj dane do tabeli Czytelnicy. Możesz modyfikować ten kod w razie potrzeby.
+            Biblioteka.BibliotekaDataSetTableAdapters.CzytelnicyTableAdapter bibliotekaDataSetCzytelnicyTableAdapter = new Biblioteka.BibliotekaDataSetTableAdapters.CzytelnicyTableAdapter();
+            bibliotekaDataSetCzytelnicyTableAdapter.Fill(bibliotekaDataSet.Czytelnicy);
+            System.Windows.Data.CollectionViewSource czytelnicyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("czytelnicyViewSource")));
+            czytelnicyViewSource.View.MoveCurrentToFirst();
         }
     }
 }
